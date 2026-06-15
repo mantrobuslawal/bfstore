108765d1f11ca69502f6abb1cd714a35af26e08c
mariam antrobus <ma.cloudsandbox@gmail.com>
2026-06-15 03:51:53 +0100

    updated basket service repository.go

--- services/basket-service/internal/basket/repository.go
+++ services/basket-service/internal/basket/repository.go
@@ -1,1 +1,11 @@
 package basket
+
+// Repository defines Basket Service persistence behaviour.
+type Repository interface {
+	CreateBasket(code CurrencyCode) (BasketID, error) // Newly created basket, return BasketID only.
+	GetBasket(id BasketID) (Basket, error)
+	AddItem(query BasketQuery) (Basket, error)
+	UpdateItemQuantity(query BasketQuery) (Basket, error)
+	RemoveItem(query BasketQuery) (Basket, error)
+	ClearBasket(id BasketID) (Basket, error)
+}
