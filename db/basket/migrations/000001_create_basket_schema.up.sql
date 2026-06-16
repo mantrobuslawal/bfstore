4059fee6317d1ae9a7c4488123bddc635fa81ed4
bitMuva <ma.cloudsandbox@gmail.com>
2026-06-16 15:07:45 +0100

    Update 000001_create_basket_schema.up.sql

--- db/basket/migrations/000001_create_basket_schema.up.sql
+++ db/basket/migrations/000001_create_basket_schema.up.sql
@@ -7,9 +7,19 @@ CREATE TABLE baskets (
   updated_at TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
 
   PRIMARY KEY (id),
+
   UNIQUE KEY uk_baskets_basket_id (basket_id),
   KEY idx_baskets_status (status),
-  KEY idx_baskets_updated_at (updated_at)
+  KEY idx_baskets_updated_at (updated_at),
+
+  CONSTRAINT chk_baskets_basket_id_format
+    CHECK (basket_id REGEXP '^basket_[0-9A-HJKMNP-TV-Z]{26}_[0-9A-HJKMNP-TV-Z]{8}$'),
+
+  CONSTRAINT chk_baskets_status
+    CHECK (status IN ('ACTIVE', 'CLEARED', 'EXPIRED', 'CHECKED_OUT')),
+
+  CONSTRAINT chk_baskets_currency_code
+    CHECK (currency_code REGEXP '^[A-Z]{3}$')
 );
 
 CREATE TABLE basket_items (
@@ -28,8 +38,14 @@ CREATE TABLE basket_items (
   updated_at TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
 
   PRIMARY KEY (id),
+
   UNIQUE KEY uk_basket_items_basket_item_id (basket_item_id),
-  UNIQUE KEY uk_basket_items_basket_product_variant (basket_id, product_id, variant_id),
+  UNIQUE KEY uk_basket_items_basket_product_variant (
+    basket_id,
+    product_id,
+    variant_id
+  ),
+
   KEY idx_basket_items_basket_id (basket_id),
   KEY idx_basket_items_product_id (product_id),
   KEY idx_basket_items_variant_id (variant_id),
@@ -37,5 +53,29 @@ CREATE TABLE basket_items (
   CONSTRAINT fk_basket_items_basket_id
     FOREIGN KEY (basket_id)
     REFERENCES baskets (basket_id)
-    ON DELETE CASCADE
+    ON DELETE CASCADE,
+
+  CONSTRAINT chk_basket_items_basket_item_id_format
+    CHECK (basket_item_id REGEXP '^bitem_[0-9A-HJKMNP-TV-Z]{26}_[0-9A-HJKMNP-TV-Z]{8}$'),
+
+  CONSTRAINT chk_basket_items_basket_id_format
+    CHECK (basket_id REGEXP '^basket_[0-9A-HJKMNP-TV-Z]{26}_[0-9A-HJKMNP-TV-Z]{8}$'),
+
+  CONSTRAINT chk_basket_items_product_id_format
+    CHECK (product_id REGEXP '^prod_[0-9A-HJKMNP-TV-Z]{26}$'),
+
+  CONSTRAINT chk_basket_items_variant_id_format
+    CHECK (variant_id REGEXP '^var_[0-9A-HJKMNP-TV-Z]{26}$'),
+
+  CONSTRAINT chk_basket_items_quantity
+    CHECK (quantity BETWEEN 1 AND 99),
+
+  CONSTRAINT chk_basket_items_unit_price_non_negative
+    CHECK (unit_price_minor_units >= 0),
+
+  CONSTRAINT chk_basket_items_line_total_non_negative
+    CHECK (line_total_minor_units >= 0),
+
+  CONSTRAINT chk_basket_items_currency_code
+    CHECK (currency_code REGEXP '^[A-Z]{3}$')
 );
