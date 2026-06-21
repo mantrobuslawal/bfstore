aaa271d6933e79b79e699d058cc9e91b8dd73303
bitMuva <ma.cloudsandbox@gmail.com>
2026-06-21 08:18:58 +0100

    Update errors.go

--- services/basket-service/internal/grpcadapter/errors.go
+++ services/basket-service/internal/grpcadapter/errors.go
@@ -15,33 +15,27 @@ func mapServiceError(err error) error {
 		errors.Is(err, basket.ErrInvalidBasketStatus),
 		errors.Is(err, basket.ErrInvalidItemID),
 		errors.Is(err, basket.ErrInvalidQuantity),
-		errors.Is(err, basket.ErrInvalidSubTotal),
+		errors.Is(err, basket.ErrInvalidSubtotal),
 		errors.Is(err, basket.ErrUnknownItemID),
-		errors.Is(err, basket.ErrInvalidCurrenyCode),
+		errors.Is(err, basket.ErrInvalidCurrencyCode),
 		errors.Is(err, basket.ErrMissingProductID),
 		errors.Is(err, basket.ErrMissingVariantID):
-
 		return status.Error(codes.InvalidArgument, err.Error())
 
 	case errors.Is(err, basket.ErrBasketNotFound),
+		errors.Is(err, basket.ErrBasketItemNotFound),
 		errors.Is(err, basket.ErrProductNotFound),
 		errors.Is(err, basket.ErrVariantNotFound):
-		return status.Error(codes.NotFound, "not found")
+		return status.Error(codes.NotFound, err.Error())
 
-	case errors.Is(err, basket.ErrProductNotSellable):
-		return status.Error(codes.FailedPrecondition, "product or variant not sellable")
-
-	case errors.Is(err, basket.ErrProductVariantMismatch):
-		return status.Error(codes.FailedPrecondition, "product and variant mismatch")
-
-	case errors.Is(err, basket.ErrBasketCurrencyMismatch):
-		return status.Error(codes.FailedPrecondition, "basket currency does not match catalog item currency")
-
-	case errors.Is(err, basket.ErrBasketNotModifiable):
-		return status.Error(codes.FailedPrecondition, "basket not modifiable")
+	case errors.Is(err, basket.ErrProductNotSellable),
+		errors.Is(err, basket.ErrProductVariantMismatch),
+		errors.Is(err, basket.ErrBasketCurrencyMismatch),
+		errors.Is(err, basket.ErrBasketNotModifiable):
+		return status.Error(codes.FailedPrecondition, err.Error())
 
 	case errors.Is(err, basket.ErrCatalogServiceUnavailable):
-		return status.Error(codes.Unavailable, "catalog service unavailable")
+		return status.Error(codes.Unavailable, err.Error())
 
 	default:
 		return status.Error(codes.Internal, "internal server error")
